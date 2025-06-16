c63208366c572b3d3b33b0f311c359a4f9e2ed71
Hemalnath21 <160467525+Hemalnath21@users.noreply.github.com>
2025-06-16 16:42:33 +0530

    Update steamlit_app.py
    adding the request

--- steamlit_app.py
+++ steamlit_app.py
@@ -14,6 +14,10 @@ st.write('The name on your smoothie will be:',name_on_smoothie)
 cnx =st.connection("snowflake")
 session =cnx.session()
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
