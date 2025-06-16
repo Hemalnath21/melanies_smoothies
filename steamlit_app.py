c4f6e2b47c1da43d7429295acb5547141e340623
Hemalnath21 <160467525+Hemalnath21@users.noreply.github.com>
2025-06-16 16:23:49 +0530

    Update steamlit_app.py
    spacing error I have changed

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,7 +11,7 @@ name_on_smoothie = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:',name_on_smoothie)
 
 # Get the current credentials
-cnx =st.connection("snowflake ")
+cnx =st.connection("snowflake")
 session =cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
